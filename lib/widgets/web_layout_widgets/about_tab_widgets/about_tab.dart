98cc12b1dce0ea6e696d812531649c6473b2bc4a
Caglar Kullu <caglarkullu@gmail.com>
2023-08-04 17:06:29 +0100

    dark-light button implemented

--- lib/widgets/web_layout_widgets/about_tab_widgets/about_tab.dart
+++ lib/widgets/web_layout_widgets/about_tab_widgets/about_tab.dart
@@ -2,6 +2,7 @@ import 'package:caglar_portfolio/consts/const.dart';
 import 'package:caglar_portfolio/consts/providers.dart';
 import 'package:caglar_portfolio/router/app_route_consts.dart';
 import 'package:caglar_portfolio/widgets/common_features/button.dart';
+import 'package:caglar_portfolio/widgets/common_features/switch_button.dart';
 import 'package:caglar_portfolio/widgets/web_layout_widgets/about_tab_widgets/me_photo.dart';
 import 'package:caglar_portfolio/widgets/web_layout_widgets/about_tab_widgets/me_writing_widget.dart';
 import 'package:caglar_portfolio/widgets/web_layout_widgets/about_tab_widgets/my_skills.dart';
@@ -21,31 +22,29 @@ class AboutPage extends ConsumerWidget {
       appBar: AppBar(
         backgroundColor: Consts.backgroundColor,
         elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            isLapTopScreen
-                ? SizedBox(
-                    width: size.width / 5,
-                  )
-                : const SizedBox(),
-            CustomButton(
-              isSelected: false,
-              label: 'About',
-              onTap: () => context.goNamed(RouteConsts.aboutRouteName),
-            ),
-            CustomButton(
-              isSelected: false,
-              label: 'Portolio',
-              onTap: () {},
-            ),
-            CustomButton(
-              isSelected: false,
-              label: 'Contact',
-              onTap: () => context.goNamed(RouteConsts.contactRouteName),
-            ),
-          ],
-        ),
+        title: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          isLapTopScreen
+              ? SizedBox(
+                  width: size.width / 5,
+                )
+              : const SizedBox(),
+          CustomButton(
+            isSelected: false,
+            label: 'About',
+            onTap: () => context.goNamed(RouteConsts.aboutRouteName),
+          ),
+          CustomButton(
+            isSelected: false,
+            label: 'Portolio',
+            onTap: () {},
+          ),
+          CustomButton(
+            isSelected: false,
+            label: 'Contact',
+            onTap: () => context.goNamed(RouteConsts.contactRouteName),
+          ),
+          const SwichButton(),
+        ]),
       ),
       body: isLapTopScreen
           ? Row(
--- lib/widgets/web_layout_widgets/web_layout.dart
+++ lib/widgets/web_layout_widgets/web_layout.dart
@@ -1,8 +1,7 @@
 import 'package:caglar_portfolio/consts/providers.dart';
 import 'package:caglar_portfolio/widgets/common_features/appbar_widget.dart';
-
 import 'package:caglar_portfolio/widgets/web_layout_widgets/about_tab_widgets/about_tab.dart';
-import 'package:caglar_portfolio/widgets/web_layout_widgets/contact_tab_widgets/contact_tab.dart';
+
 import 'package:caglar_portfolio/widgets/web_layout_widgets/portfolio_widgets/portfolio_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,33 +13,10 @@ class WebLayout extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Size size = ref.refresh(sizeProviderProvider(context));
-    final aboutKey = ref.watch(aboutKeyProvider);
-    final portfolioKey = ref.watch(portfolioKeyProvider);
-    final contactKey = ref.watch(contactKeyProvider);
-    return Scaffold(
-        appBar: AppBar(
-          flexibleSpace: AppBarWidget(
-            webController: _controller,
-            mobileController: null,
-          ),
-        ),
-        body: ListView(
-          controller: _controller,
-          children: [
-            SizedBox(
-              width: size.width / 2,
-              height: size.height,
-              child: AboutPage(
-                key: aboutKey,
-              ),
-            ),
-            PortfolioPage(
-              key: portfolioKey,
-            ),
-            ContactPage(
-              key: contactKey,
-            )
-          ],
-        ));
+    return const Scaffold(
+      body: Center(
+        child: AboutPage(),
+      ),
+    );
   }
 }
